ea6be8b236070a26d3bffa4778b3132c4c80b1bb
Peter Froehlich <peter.hans.froehlich@gmail.com>
2009-12-12 11:49:12 -0500

    Draft ResultSet implementation, not used yet.

--- core.go
+++ core.go
@@ -587,3 +587,67 @@ func (self *ClassicResultSet) Fetch() (result db.Result) {
 func (self *ClassicResultSet) Close() os.Error {
 	return nil;
 }
+
+
+
+
+
+
+type ResultSet struct {
+	// we implement everything in terms of classic stuff
+	classic *ClassicResultSet;
+	// channel to send results through
+	results chan db.Result;
+	// channel to check for termination
+	stops chan bool;
+}
+
+func (self *ResultSet) init(crs *ClassicResultSet) {
+	self.classic = crs;
+	self.results = make(chan db.Result);
+	self.stops = make(chan bool);
+}
+
+func (self *ResultSet) kill() {
+	self.classic.Close();
+	self.classic = nil;
+	close(self.results);
+	self.results = nil;
+	close(self.stops);
+	self.stops = nil;
+}
+
+// goroutine implementing the iterator
+func (self *ResultSet) iterate() {
+	for self.classic.More() {
+		// block until either send or receive
+		select {
+		case self.results <- self.classic.Fetch():
+			fmt.Printf("sent to %s", self.results);
+		case _ = <-self.stops:
+			fmt.Printf("received from %s", self.stops);
+			break;
+		default:
+			fmt.Printf("debugging: no communication");
+		}
+	}
+	self.kill();
+}
+
+func (self *ResultSet) Iter() <-chan db.Result {
+	go self.iterate();
+	return self.results;
+}
+
+func (self *ResultSet) Close() os.Error {
+	self.stops <- true;
+	return nil;
+}
+
+func (self *ResultSet) Names() []string {
+	return nil;
+}
+
+func (self *ResultSet) Types() []string {
+	return nil;
+}
